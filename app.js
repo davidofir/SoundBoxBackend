b2debd88bdf0d724bbbbe454faf517a48278baea
ofir <davidof@sheridancollege.ca>
2022-11-19 18:21:30 -0500

    fixed the port

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const app = express();
 const cors = require('cors');
-const PORT = process.env.PORT || 8080;
+
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
 const { getCatalog } = require("merchnow-api-node")
-
+const PORT = process.env.PORT || 8080;
 app.get('/:artist',cors(),async(req,res)=>{
 
     const results = await getCatalog(req.params.artist)
