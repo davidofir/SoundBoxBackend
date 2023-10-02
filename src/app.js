e57678b98d09c6bc51275fedd653a2eb590d0d09
ofir <davidof@sheridancollege.ca>
2023-10-02 00:32:48 -0400

    migration fix add puppeteer

--- src/app.js
+++ src/app.js
@@ -1,19 +1,29 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 const cors = require('cors');
+const puppeteer = require('puppeteer');
+const merchRepo = require('./repositories/MerchRepo');
+const http = require('http');
+const server = http.createServer(app);
 
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
-const { getCatalog } = require("merchnow-api-node")
+const { getCatalog } = require('merchnow-api-node');
 const PORT = process.env.PORT || 8080;
-app.get('/:artist',cors(),async(req,res)=>{
 
-    const results = await getCatalog(req.params.artist)
-    console.log(results)
-    await res.json(results)
-})
+app.get('/merchnow/:artist', cors(), async (req, res) => {
+  const results = await getCatalog(req.params.artist);
+  console.log(results);
+  await res.json(results);
+});
 
-app.listen(PORT,()=>{
-    console.log(`app is listening on port ${PORT}`);
-})
+app.get('/topic/:artist', cors(), async (req, res) => {
+  const results = await merchRepo.getHotTopicMerch(req.params.artist);
+  res.json(results);
+});
+
+
+server.listen(PORT, () => {
+  console.log(`app is listening on port ${PORT}`);
+});
