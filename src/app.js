c9e5476ee232dfa9f0c925423cbf26a6d8796298
ofir <davidof@sheridancollege.ca>
2023-12-15 14:58:02 -0500

    removed unnecessary code

--- src/app.js
+++ src/app.js
@@ -14,14 +14,7 @@ const { admin, db } = require('./firebase');  // Import from your config file
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const { getCatalog } = require('merchnow-api-node');
 const PORT = process.env.PORT || 8080;
-
-app.get('/merchnow/:artist', cors(), async (req, res) => {
-  const results = await getCatalog(req.params.artist);
-  console.log(results);
-  await res.json(results);
-});
 
 app.get('/topic/:artist', cors(), async (req, res) => {
   const results = await merchRepo.getHotTopicMerch(req.params.artist);
